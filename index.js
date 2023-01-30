644a7cfc10bc965993237d38b863ee3cf022739c
afsarjahan ansari <nagma1804@gmail.com>
2023-01-30 10:23:35 +0530

    adding files to repository

---
+++ index.js
@@ -0,0 +1,16 @@
+let qrText = document.getElementById('qrText');
+let qrImage = document.getElementById('qrImage');
+let imgBox = document.getElementById('imgBox');
+
+function generateQR(){
+    if(qrText.value.length > 0){
+        qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}`;
+        imgBox.classList.add('show-img');
+    }else{
+        imgBox.classList.remove('show-img');
+        qrText.classList.add('error');
+        setTimeout(() => {
+            qrText.classList.remove('error');
+        }, 1000)
+    }
+}
